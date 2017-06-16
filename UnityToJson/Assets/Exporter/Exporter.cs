a1d5b9b861f68ea2e1dca38dd1f1fdd7272727a0
shengchongshan <shengchongshan@sensetime.com>
2017-06-17 02:08:43 +0800

    add json serialize

--- UnityToJson/Assets/Exporter/Exporter.cs
+++ UnityToJson/Assets/Exporter/Exporter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Exporter {
+public class Exporter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- UnityToJson/Assets/Exporter/ToJsonTrailRenderer.cs
+++ UnityToJson/Assets/Exporter/ToJsonTrailRenderer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ToJsonTrailRenderer : MonoBehaviour {
+public class ToJsonTrailRenderer {
 
 	// Use this for initialization
 	void Start () {
